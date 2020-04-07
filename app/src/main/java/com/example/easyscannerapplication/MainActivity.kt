14200eb9dd24213892d6b40d319853355dc0fbc6
Agnieszka Różańska <agniroz767@student.polsl.pl>
2020-04-07 14:45:23 +0200

    Implementation of the main functions that enable code scanning

---
+++ app/src/main/java/com/example/easyscannerapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.easyscannerapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnScan.setOnClickListener {
+            run {
+                IntentIntegrator(this@MainActivity).initiateScan();
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        var result: IntentResult? = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
+        if(result != null){
+
+            if(result.contents != null){
+                txtValue.text = result.contents
+            } else {
+                txtValue.text = getString(R.string.scanfailed)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/easyscannerapplication/MultiDexApp.kt
@@ -0,0 +1,10 @@
+package com.example.easyscannerapplication
+
+import androidx.multidex.MultiDexApplication
+
+class MultiDexApp : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
